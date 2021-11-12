95177bf54ea7007c74717ce282970e5bb5674c1e
jay_wu <jay_wu>
2021-11-13 06:45:16 +0800

    add base page (get jwt and store on local storage)

--- food-app/src/App.js
+++ food-app/src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
+const apiUrl = 'http://localhost:3001';axios.interceptors.request.use(
+  config => {
+    const { origin } = new URL(config.url);
+    const allowedOrigins = [apiUrl];
+    const token = localStorage.getItem('token');
+    
+    if (allowedOrigins.includes(origin)) {
+      config.headers.authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
+
 function App() {
+  const storedJwt = localStorage.getItem('token');
+  const [jwt, setJwt] = useState(storedJwt || null);
+  const [foods, setFoods] = useState([]);
+  const [fetchError, setFetchError] = useState(null);
+  
+  const getJwt = async () => {
+    const { data } = await axios.get(`${apiUrl}/jwt`);
+    localStorage.setItem('token', data.token);
+    setJwt(data.token);
+  };
+  
+  const getFoods = async () => {
+    try {
+      const { data } = await axios.get(`${apiUrl}/foods`);
+      setFoods(data);
+      setFetchError(null);
+    } catch (err) {
+      setFetchError(err.message);
+    }
+  };
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section style={{ marginBottom: '10px' }}>
+        <button onClick={() => getJwt()}>Get JWT</button>
+        {jwt && (
+          <pre>
+            <code>{jwt}</code>
+          </pre>
+        )}
+      </section>
+      <section>
+        <button onClick={() => getFoods()}>
+          Get Foods
+        </button>
+        <ul>
+          {foods.map((food, i) => (
+            <li key={food.id}>{food.description}</li>
+          ))}
+        </ul>
+        {fetchError && (
+          <p style={{ color: 'red' }}>{fetchError}</p>
+        )}
+      </section>
+    </>
   );
 }
 
