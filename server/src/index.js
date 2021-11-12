b2eae3a51a12c7cb8b715fc44dffbd51af931b25
jay_wu <jay_wu>
2021-11-13 07:04:34 +0800

    Refactor to Store JWT in a Cookie - 3 Validate the JWT from the Cookie

--- server/src/index.js
+++ server/src/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const jwt = require('express-jwt');
 const jsonwebtoken = require('jsonwebtoken');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
 app.use(cors());
+app.use(cookieParser());
 
 const jwtSecret = 'secret123';
 
@@ -13,13 +15,19 @@ app.get('/jwt', (req, res) => {
   const token = jsonwebtoken.sign({ user: 'johndoe' }, jwtSecret);
 
   res.cookie('token', token, { httpOnly: true });
-  
+
   res.json({
     token: jsonwebtoken.sign({ user: 'johndoe' }, jwtSecret)
   });  
 });
 
-app.use(jwt({ secret: jwtSecret, algorithms: ['HS256'] }));
+app.use(
+  jwt({
+    secret: jwtSecret,
+    getToken: req => req.cookies.token,
+    algorithms: ['HS256'],
+  })
+);
 
 const foods = [
   { id: 1, description: 'burritos' },
